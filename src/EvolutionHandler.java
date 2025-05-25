61f7303820ca08f77ceb56a55f7f004dd0bc189f
Kidokun0317 <cocoingdom@gmail.com>
2025-05-25 20:33:39 +0900

    Delete src directory

--- src/EvolutionHandler.java
+++
@@ -1,6 +0,0 @@
-public class EvolutionHandler {
-    public static void applyLevel1Evolutions() {
-        // 여기에 진화 조건을 레벨 1로 고정하고 최종진화 제거하는 로직 구현
-        System.out.println("모든 포켓몬을 1레벨에서 진화하게 설정합니다. 최종진화는 제외됩니다.");
-    }
-}
--- src/Main.java
+++
@@ -1,5 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("PokeRandoZX Custom 시작됨 - 여기에 1레벨 진화/아이템 치환 로직 구현됨");
-    }
-}
